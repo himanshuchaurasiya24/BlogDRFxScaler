a32c3ab668c720bacfed7021da6ce50899846675
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-09-26 12:41:00 +0530

    login functionality working fine

---
+++ account/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from rest_framework import status
+class RegisterView(APIView):
+    def post(self, request):
+        data= request.data
+        serializer= RegisterSerializer(data=data)
+        try:
+            if not serializer.is_valid():
+                return Response({'message':'somthing went wrong',
+                "data":serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response({'message':'account has been created.','data':serializer.data}, 
+            status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({'message':'some error has occured.'}, status= status.HTTP_400_BAD_REQUEST)
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer= LoginSerializer(data=data)
+            if not serializer.is_valid():
+                return Response({
+                    'data':serializer.errors,
+                    'message':'somthing went wrong.'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            response = serializer.get_jwt_token(serializer.data)
+            return Response(response, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            print(e)
+            return Response({'data':{},
+                             'message':'somthing went wrong in exception.'
+            }, status=status.HTTP_400_BAD_REQUEST)
