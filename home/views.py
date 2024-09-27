0064557130ba88bd09552f90c383facbf16b14bd
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-09-27 21:24:51 +0530

    get_method in blog view.py

--- home/views.py
+++ home/views.py
@@ -1,12 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import BlogSerializer
+from .models import *
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 class BlogView(APIView):
     permission_classes=[IsAuthenticated]
     authentication_classes=[JWTAuthentication]
+    def get(self, request):
+        object = Blog.objects.all()
+        serializer = BlogSerializer(object, many=True)
+        return Response (serializer.data)
+    # permission_classes=[IsAuthenticated]
+    # authentication_classes=[JWTAuthentication]
     def post(self, request):
         try:
             data = request.data
@@ -18,3 +25,5 @@ class BlogView(APIView):
             return Response()
         except Exception as e:
             print(e)
+
+
